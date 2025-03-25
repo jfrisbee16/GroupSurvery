37f0d06d1bc95ecf805e003dc63cfd010a66785f
John Frisbee <jfrisbee16@gmail.com>
2025-03-24 19:54:57 -0500

    fixed sweetAlerts

--- scripts/login.js
+++ scripts/login.js
@@ -1,37 +1,34 @@
 // Validation for Login Form
 document.querySelector("#btnLogin").addEventListener("click", (e) => {
-    e.preventDefault() // Prevent form submission
+    e.preventDefault(); // Prevent form submission
     
-    const strEmail = document.querySelector("#strUsername").value.trim()
-    const strPassword = document.querySelector("#strPassword").value.trim()
-    const regEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i
-    const regPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
-    let errors = ""
+    const strEmail = document.querySelector("#strUsername").value.trim();
+    const strPassword = document.querySelector("#strPassword").value.trim();
+    const regEmail = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
+    const regPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    let errors = "";
 
-    // Validate the data
     let blnError = false;
     let strMessage = '';
-    if (!regEmail.test(strEmail)){
+    if (!regEmail.test(strEmail)) {
         blnError = true;
-        strMessage += '<p>Please enter a valid email address.\n</p>';
+        strMessage += '<p>Please enter a valid email address.</p>';
     } else if (!regPassword.test(strPassword)) {
         blnError = true;
-        strMessage += '<p>Please enter a valid password.\n</p>';
+        strMessage += '<p>Please enter a valid password.</p>';
     }
 
-    if(blnError == true) {
+    if (blnError) {
         Swal.fire({
             title: "Error",
             html: strMessage,
             icon: 'error',
-        })
+        });
     } else {
         Swal.fire({
             title: "Success!",
             text: "Login Successful!",
             icon: "success"
-        })
+        });
     }
-
-    
-})
+});
