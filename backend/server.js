0b569ee6511049ea4fc8cc8476aebbcfaf5c6794
CristofherJT <156453145+CristofherJT@users.noreply.github.com>
2025-04-30 20:07:13 -0500

    Update server.js
    Updated the post for registration for full name and the command

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,25 @@ var app = express()
 app.use(cors())
 app.use(express.json())
 
+//Need a role and change user id to email in the database
+//Use this to in registration.js to post to the server for the backend
+// fetch("/user", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//       email: "user@example.com",
+//       password: "MyPassword123!"
+//     })
+//   });
+
 app.post('', (req, res, next) => {
     let strEmail = req.body.email.trim().toLowerCase();
     let strPassword = req.body.password;
+    let strFirstName = req.body.firstName.trim();
+    let strLastName = req.body.lastName.trim();
+    let strRole = req.body.role;
 
     //Email validation
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -39,11 +55,21 @@ app.post('', (req, res, next) => {
         return res.status(400).json({ error: "Password must contain at least one special character" });
     }
 
+    //First Name validation
+    if (strFirstName.length < 3) {
+        return res.status(400).json({ error: "First name must be at least 3 characters long" });
+    }
+
+    //Last Name validation
+    if (strLastName.length < 3) {
+        return res.status(400).json({ error: "Last name must be at least 3 characters long" });
+    }
+
     strPassword = bcrypt.hashSync(strPassword, intSalt);
 
     // If validations pass
-    let strCommand = `INSERT INTO tblUsers VALUES (?, ?, ?)`;
-    db.run(strCommand, [strEmail, strPassword, "Active"], function (err) {
+    let strCommand = `INSERT INTO tblUsers VALUES (?, ?, ?, ?)`;
+    db.run(strCommand, [strEmail, strFirstName, strLastName, strPassword], function (err) {
         if(err){
             console.log(err)
             res.status(400).json({
